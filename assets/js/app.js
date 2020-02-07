2af3bd86e256b82e682c81f1952620aa49792e3f
Amanda Higgins <amanda.n.higgins@gmail.com>
2020-02-07 18:02:30 -0500

    starting to add code

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAJTS4cs33sqS8A87N_CqKZV4nD5RDFeZg",
+    authDomain: "my-awesome-project-55813.firebaseapp.com",
+    databaseURL: "https://my-awesome-project-55813.firebaseio.com",
+    projectId: "my-awesome-project-55813",
+    storageBucket: "my-awesome-project-55813.appspot.com",
+    messagingSenderId: "186919523450",
+    appId: "1:186919523450:web:0cdf3260d19d65fbad97c3"
+  };
+  firebaseConfig.initializeApp(config);
+
+  // database reference
+  var database = firebase.database();
+
+  // Click
+  $("#submit").on("click", function(event) {
+    event.preventDefault();
