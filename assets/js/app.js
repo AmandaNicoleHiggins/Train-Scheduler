985421b8eaf6c1a8218085b585eaeeffea4605ce
Amanda Higgins <amanda.n.higgins@gmail.com>
2020-02-10 13:10:23 -0500

    finished

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 
 // firebase code
-const firebaseConfig = {
+var firebaseConfig = {
     apiKey: "AIzaSyAJTS4cs33sqS8A87N_CqKZV4nD5RDFeZg",
     authDomain: "my-awesome-project-55813.firebaseapp.com",
     databaseURL: "https://my-awesome-project-55813.firebaseio.com",
@@ -9,7 +9,8 @@ const firebaseConfig = {
     messagingSenderId: "186919523450",
     appId: "1:186919523450:web:0cdf3260d19d65fbad97c3"
   };
-  firebaseConfig.initializeApp(config);
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
   // database reference
   var database = firebase.database();
@@ -18,11 +19,16 @@ const firebaseConfig = {
   $("#submit").on("click", function(event) {
     event.preventDefault();
 
+    var trainName = $("#trainName-input").val().trim();
+    var destination = $("#destination-input").val().trim();
+    var firstTime = $("#firstTime-input").val().trim();
+    var frequency = $("#frequency-input").val().trim();
+
     // Push
     // creates object for holding data
     var newTrain = {
         trainName: trainName,
-        desitnation: desitnation,
+        destination: destination,
         firstTime: firstTime,
         frequency: frequency,
     };
@@ -30,3 +36,47 @@ const firebaseConfig = {
     database.ref().push(newTrain);
 });
 
+database.ref().on("child_added", function(childSnapshot) {
+    var trainName = childSnapshot.val().trainName;
+    var destination = childSnapshot.val().destination;
+    var firstTime = childSnapshot.val().firstTime;
+    var frequency = childSnapshot.val().frequency;
+
+    // console.log(trainName);
+    // console.log(destination);
+    // console.log(firstTime);
+    // console.log(frequency);
+
+    //first time
+    var firstTimeConvert = moment(firstTime, "HH:mm").subtract(1, "years");
+    console.log(firstTimeConvert)
+
+    //current time
+    var currentTime = moment();
+
+    //time difference
+    var timeDiff = moment().diff(moment(firstTimeConvert), "minutes");
+
+    //remainder
+    var remainder = timeDiff % frequency;
+
+    //minutes til train
+    var minAway = frequency - remainder;
+
+    //next traun
+    var nextArrive = moment().add(minAway, "minutes");
+    var nextTrain = nextArrive.format("hh:mm");
+
+    //add to html
+    var newRow = $("<tr>").append(
+        $("<td>").text(trainName),
+        $("<td>").text(destination),
+        $("<td>").text(frequency),
+        $("<td>").text(nextTrain),
+        $("<td>").text(minAway)
+    );
+
+    $("#schedule > tbody").append(newRow);
+}, function(errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+});
